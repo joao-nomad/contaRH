c586e8c2cbc73d25f375870aa0b7e3511bcc00a8
João Vitor Lessa <jvcadastro@gmail.com>
2016-08-21 22:27:42 -0300

    Almost

--- js/formspree.js
+++ js/formspree.js
@@ -2,7 +2,7 @@ var $contactForm = $('#contact-form');
 	$contactForm.submit(function(e) {
 	e.preventDefault();
 	$.ajax({
-		url: 'https://formspree.io/joaovitorlessa@gmail.com',
+		url: 'https://formspree.io/contato.contarh@hotmail.com',
 		method: 'POST',
 		data: $(this).serialize(),
 		dataType: 'json',
