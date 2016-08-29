349d21f9d51d1c81532e207a86d5cb3350f854bf
João Vitor Lessa <jvcadastro@gmail.com>
2016-08-28 22:25:16 -0300

    Subindo serviços empresariais

--- js/mainEmpresas.js
+++ js/mainEmpresas.js
@@ -53,6 +53,80 @@ $("#civilLink").click(function(e){
     $("#servImg").addClass("civilImg");
 });
 
+$("#educaLink").click(function(e){
+    $("#supermLink, #combustLink, #atacadistaLink, #varejistaLink, #condoLink, #civilLink, #educaLink, #hoteisLink, #industriasLink, #shoppingLink, #prestLink, #agroLink, #saudeLink, #liberalLink").parent().removeClass('active');
+    $("#educaLink").parent().addClass('active');
+    $("#tituloServ").text("Educação");
+    $("#descServ").text("O cenário educacional tem passado por expressivas mudanças nos últimos tempos. Mudanças no cenário econômico e em políticas públicas, tem exigido deste segmento uma expressiva melhora da qualidade de ensino ofertado, além de impor um aumento crescente em seus custos e mecanismos de controle. Tudo isto combinado com a redução do financiamento estudantil público, como é o caso do Fies, e dos programas estaduais, impõem ao segmento a adoção de melhores práticas de gestão. Neste sentido, os serviços que prestamos estão no centro deste processo. E para atender todas estas demandas, o produto Contabilidade Educacional é apresentado no formato de “módulos”, cada um com características bem definidas, e que podem ser oferecidos ao mercado educacional como um “Lego”, onde definimos junto aos nossos clientes o que está mais aderente ao seu modelo de gestão, momento e porte. Expertise no atendimento de IES, Colégios, cursos livres e Caixa Escolar.");
+    $("#servImg").removeClass();
+    $("#servImg").addClass("educImg");
+});
+
+
+$("#hoteisLink").click(function(e){
+    $("#supermLink, #combustLink, #atacadistaLink, #varejistaLink, #condoLink, #civilLink, #educaLink, #hoteisLink, #industriasLink, #shoppingLink, #prestLink, #agroLink, #saudeLink, #liberalLink").parent().removeClass('active');
+    $("#hoteisLink").parent().addClass('active');
+    $("#tituloServ").text("Hotéis, Bares e Restaurantes");
+    $("#descServ").text("O segmento que reúne Hotéis, Bares e Restaurantes, além oscilação da margem de lucro em função da variação sazonal dos custos dos insumos e dos riscos inerentes à atividade, vem sendo cada vez mais visado pelos órgãos de fiscalização e sindicatos. Para estas empresas, considerando toda a regulação normativa específica para o segmento, com destaque para a área trabalhista e fiscal, além do advento de novas práticas como as normas de contabilidade internacional (IFRS), SPED’s e e-Social, a NTW Contabilidade e Gestão Empresarial desenvolveu uma metodologia contábil customizada para o setor. Nossas soluções são apesentadas no formato de “módulos”, cada um com características específicas, que serão definidas considerando as expectativas, o modelo de gestão, o momento e o porte de cada cliente. Expertise no atendimento de Bares, Restaurantes, Pizzarias, Lanchonetes, Hoteis,, Moteis, Pousadas, Pub’s, Cinemas.");
+    $("#servImg").removeClass();
+    $("#servImg").addClass("hotelImg");
+});
+
+$("#industriasLink").click(function(e){
+    $("#supermLink, #combustLink, #atacadistaLink, #varejistaLink, #condoLink, #civilLink, #educaLink, #hoteisLink, #industriasLink, #shoppingLink, #prestLink, #agroLink, #saudeLink, #liberalLink").parent().removeClass('active');
+    $("#industriasLink").parent().addClass('active');
+    $("#tituloServ").text("Indústrias");
+    $("#descServ").text("O segmento industrial vem sofrendo com margens de lucro cada vem mais apertadas, consequência de políticas econômicas de curto prazo que já projetam para 2016 um crescimento negativo, falta de financiamento além de uma das maiores taxas de juros do mundo, variações cambiais, concorrência internacional e forte regulação fiscal, trabalhista e contábil. Para o segmento Industrial, considerando toda a regulação normativa específica para o segmento que exige especial atenção tributária em especial do IPI e ICMS; um contabilidade focada em custos e controle de ativos; além do advento de novas práticas como as normas de contabilidade internacional (IFRS), SPED, Bloco K e e-Social, a NTW Contabilidade e Gestão Empresarial desenvolveu uma metodologia contábil customizada para o setor. Nossas soluções são apresentadas no formato de “módulos”, cada um com características específicas, que serão definidas considerando as expectativas, o modelo de gestão, o momento e o porte de cada cliente. Expertise no atendimento de Construtoras, Empreiteiras, Incorporadoras, Sistemas de construção em condomínio fechado, empresas de Engenharia e Arquitetura.");
+    $("#servImg").removeClass();
+    $("#servImg").addClass("industImg");
+});
+
+$("#shoppingLink").click(function(e){
+    $("#supermLink, #combustLink, #atacadistaLink, #varejistaLink, #condoLink, #civilLink, #educaLink, #hoteisLink, #industriasLink, #shoppingLink, #prestLink, #agroLink, #saudeLink, #liberalLink").parent().removeClass('active');
+    $("#shoppingLink").parent().addClass('active');
+    $("#tituloServ").text("Lojas de Shopping");
+    $("#descServ").text("O número de shopping centers vem crescendo em todo o país. Em 2014 a ABRASCE totalizou 9.242 estabelecimentos comerciais em malls, tendo registrado um aumento de 10,4% em relação ao ano anterior. A implantação e gestão de Lojas em Shopping, no entanto, tem especificidades que devem ser discutidas e monitoradas pelo lojista com o apoio do contador, possibilitando um maior controle dos custos e redução de riscos fiscais e trabalhistas. Para estas empresas, considerando o alto investimento na implantação, as características dos contratos de locação, além do advento de novas práticas como as normas de contabilidade internacional (IFRS), SPED’s e e-Social, a NTW Contabilidade e Gestão Empresarial desenvolveu uma metodologia contábil customizada para o setor. Nossas soluções são apresentadas no formato de “módulos”, cada um com características específicas, que podem ser contratadas considerando as expectativas, o modelo de gestão, o momento e o porte de cada cliente. Além de contabilidade, temos soluções específicas para planejamento e monitoramento de perfomance de redes de negócios.");
+    $("#servImg").removeClass();
+    $("#servImg").addClass("shopImg");
+});
+
+$("#prestLink").click(function(e){
+    $("#supermLink, #combustLink, #atacadistaLink, #varejistaLink, #condoLink, #civilLink, #educaLink, #hoteisLink, #industriasLink, #shoppingLink, #prestLink, #agroLink, #saudeLink, #liberalLink").parent().removeClass('active');
+    $("#prestLink").parent().addClass('active');
+    $("#tituloServ").text("Prestadores de Serviços");
+    $("#descServ").text("Para quem exerce sua atividade como PJ, seja por uma exigência do contratante ou por opção, preparamos uma solução adequada ao seu porte, que garante redução de risco e agilidade no atendimento. Já para quem ainda é Autônomo e tem dúvidas se é melhor abrir uma empresa ou ficar como está, nossa primeira preocupação será assessorá-lo nesta decisão, simulando os custos de cada opção e esclarecendo sobre todas as obrigações de cada caso. Nas duas situações a segurança do cliente é garantida por nossa atenção cuidadosa sobre alguns processos, tais como: Comprovação de renda • Acompanhamento tributário para redução de impostos • Cumprimento das obrigações fiscais e tributárias • Segurança e controle das informações enviadas à Receita Federal do Brasil. Para atender esta demanda oferecemos uma consultoria permanente nas áreas gerenciais do negócio, financeira, contábil, fiscal e trabalhista. Nossos produtos podem ser contratados por módulos, considerando a expectativa e o porte de cada cliente.");
+    $("#servImg").removeClass();
+    $("#servImg").addClass("pservicoImg");
+});
+
+
+$("#agroLink").click(function(e){
+    $("#supermLink, #combustLink, #atacadistaLink, #varejistaLink, #condoLink, #civilLink, #educaLink, #hoteisLink, #industriasLink, #shoppingLink, #prestLink, #agroLink, #saudeLink, #liberalLink").parent().removeClass('active');
+    $("#agroLink").parent().addClass('active');
+    $("#tituloServ").text("Agronegócio");
+    $("#descServ").text("O agronegócio é um dos poucos segmentos que vem crescendo no país, respondendo por quase ¼ do PIB Brasileiro (CNA – Confederação da Agricultura e Pecuária do Brasil). A NTW Contabilidade vem desenvolvendo soluções focadas nas exigências do setor, e implantando escritórios em alguns dos maiores estados produtores do país. Nossos produtos podem ser contratados por módulos, considerando a expectativa e o porte de cada cliente. Expertise no atendimento de Empresas rurais, grande produtor equiparado a empresa, pequeno produtor.");
+    $("#servImg").removeClass();
+    $("#servImg").addClass("agroImg");
+});
+
+$("#saudeLink").click(function(e){
+    $("#supermLink, #combustLink, #atacadistaLink, #varejistaLink, #condoLink, #civilLink, #educaLink, #hoteisLink, #industriasLink, #shoppingLink, #prestLink, #agroLink, #saudeLink, #liberalLink").parent().removeClass('active');
+    $("#saudeLink").parent().addClass('active');
+    $("#tituloServ").text("Profissionais e Empresas de Saúde");
+    $("#descServ").text("Devido às crescentes exigências, a NTW vem desenvolvendo soluções contábeis customizadas para prover Hospitais, Clínicas e Profissionais da área da saúde, com relatórios e indicadores de desempenho contábeis e financeiros de uso diário e permanente, que possibilitem análises reflexivas sobre o seu desempenho, facilitando tomadas de decisões, buscando a maximização dos resultados e minimização dos custos com segurança. O segmento exige atenção redobrada sobre alguns processos tais como • Comprovação de renda • Acompanhamento tributário para redução de impostos • Cumprimento das obrigações fiscais e tributárias • Segurança e controle das informações enviadas à Receita Federal do Brasil. Para atender esta demanda oferecemos uma consultoria permanente nas áreas gerenciais do negócio, financeira, contábil, fiscal e trabalhista.  Considerando momento e o porte de nossos clientes, o produto para a área de Saúde é apresentado no formato de módulos:  o cliente escolhe o que mais está alinhado com sua necessidade no momento. Um serviço exclusivo e diferenciado para prescrever ações precisas e lucros consistentes. O que você está esperando? Agende uma consulta e conheça mais sobre os serviços que podemos receitar para a saúde de suas contas.");
+    $("#servImg").removeClass();
+    $("#servImg").addClass("saudeImg");
+});
+
+$("#liberalLink").click(function(e){
+    $("#supermLink, #combustLink, #atacadistaLink, #varejistaLink, #condoLink, #civilLink, #educaLink, #hoteisLink, #industriasLink, #shoppingLink, #prestLink, #agroLink, #saudeLink, #liberalLink").parent().removeClass('active');
+    $("#liberalLink").parent().addClass('active');
+    $("#tituloServ").text("Profissionais Liberais");
+    $("#descServ").text("Atender a um Profissional Liberal é entender que, sob a perspectiva contábil, fiscal e financeira,  ele deve receber a mesma atenção que uma empresa. Com o aprimoramento dos mecanismos de controle e fiscalização, tais como a obrigatoriedade de informação de CPF dos usuários de serviços, o cruzamento de dados com gastos de cartão de crédito (DECRED), transações imobiliárias informadas pelos cartórios (DIMOB ou DOI), ou compra e venda de carros, motos, embarcações e aviões (DETRAN’S, ANAC e Cia do Portos); além de novas obrigações instituídas como é o caso do  e-social, aumentam a necessidade de rigor técnico no planejamento e execução da contabilidade destes profissionais. Para garantir maior segurança aos nossos clientes dedicamos especial atenção sobre alguns processos, tais como: Comprovação de renda • Acompanhamento tributário para redução de impostos • Cumprimento das obrigações fiscais e tributárias • Segurança e controle das informações enviadas à Receita Federal do Brasil. No atendimento a estas demandas oferecemos ao nossos clientes uma consultoria permanente nas áreas financeira, contábil, fiscal e trabalhista. Expertise no atendimento de profissionais que atuam como advogados, consultores, titulares de  cartórios e outras profissões regulamentadas. Nossos produtos podem ser contratados por módulos, considerando a expectativa e o porte de cada cliente.");
+    $("#servImg").removeClass();
+    $("#servImg").addClass("liberalImg");
+});
+
 // Closes the sidebar menu
 $("#menu-close").click(function(e) {
     //e.preventDefault();
