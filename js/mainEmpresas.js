d2b907f83825ed19787fe52ba455428e04bf30a9
João Vitor Lessa <jvcadastro@gmail.com>
2016-08-28 22:01:03 -0300

    Subindo serviços empresariais

--- js/mainEmpresas.js
+++ js/mainEmpresas.js
@@ -1,3 +1,58 @@
+//Serv Button
+$("#supermLink").click(function(e){
+    $("#supermLink, #combustLink, #atacadistaLink, #varejistaLink, #condoLink, #civilLink, #educaLink, #hoteisLink, #industriasLink, #shoppingLink, #prestLink, #agroLink, #saudeLink, #liberalLink").parent().removeClass('active');
+    $("#supermLink").parent().addClass('active');
+    $("#tituloServ").text("Supermercado");
+    $("#descServ").text("Para a contabilidade de Supermercados, em função do volume e complexidade de suas operações, além do advento de novas práticas como as normas de contabilidade internacional (IFRS), SPED’s e e-Social, a NTW Contabilidade e Gestão Empresarial desenvolveu uma metodologia contábil especificamente concebida  para  aplicação a  empresas de supermercados e tributação pelo lucro real por estimativa mensal, com base em balancete de redução ou suspensão. E para atender todas estas demandas, esta solução contábil é apresentada no formato de “módulos”, cada um com características bem definidas, que serão avaliadas junto aos nossos clientes o seu modelo de gestão, momento e porte.");
+    $("#servImg").removeClass();
+    $("#servImg").addClass("mercImg");
+});
+
+$("#combustLink").click(function(e){
+    $("#supermLink, #combustLink, #atacadistaLink, #varejistaLink, #condoLink, #civilLink, #educaLink, #hoteisLink, #industriasLink, #shoppingLink, #prestLink, #agroLink, #saudeLink, #liberalLink").parent().removeClass('active');
+    $("#combustLink").parent().addClass('active');
+    $("#tituloServ").text("Postos de Combustíveis");
+    $("#descServ").text("Com exigências crescentes, e margens de lucro cada vez mais apertadas, a gestão de postos de combustíveis vem se profissionalizando. Considerando além da complexidade de suas operações e do advento de novas práticas como as normas de contabilidade internacional (IFRS), SPED’s e e-Social, e ainda obrigações acessórias junto a ANP, INMETRO e outros órgãos fiscalizadores do ramo, a NTW Contabilidade e Gestão Empresarial desenvolveu uma solução especificamente concebida  para  aplicação a postos de combusíveis e lojas de conveniência. E para atender todas estas demandas, nossa solução contábil é apresentada no formato de “módulos”, cada um com características bem definidas, que serão avaliadas junto aos nossos clientes considerando o seu modelo de gestão, momento e porte.");
+    $("#servImg").removeClass();
+    $("#servImg").addClass("combImg");
+});
+
+$("#atacadistaLink").click(function(e){
+    $("#supermLink, #combustLink, #atacadistaLink, #varejistaLink, #condoLink, #civilLink, #educaLink, #hoteisLink, #industriasLink, #shoppingLink, #prestLink, #agroLink, #saudeLink, #liberalLink").parent().removeClass('active');
+    $("#atacadistaLink").parent().addClass('active');
+    $("#tituloServ").text("Comércio Atacadista");
+    $("#descServ").text("Com mais de 160 mil empresas atacadistas de todos os portes espalhadas pelo Brasil, este é um dos segmentos que tem merecido especial atenção da NTW. Este segmento tem como características, dentre outros, transações com valores mais elevados, grande movimentação interestadual de mercadorias, volume de operações de ICMS em substituição tributária e constante alteração na legislação específica. Algumas destas empresas, em função da origem ou do produto que movimentam, podem ainda ser equiparadas a indústrias para efeito de cobrança do IPI, mesmos nos casos em que não há, diretamente, operações de industrialização. Tudo isto, além de aspectos societários, dentre outros, deve ser observado na correta opção do regime tributário. Tudo isto aliado às novas práticas como as normas de contabilidade internacional (IFRS), SPED, Bloco K e e-Social, impulsionou a NTW Contabilidade e Gestão Empresarial a desenvolver uma metodologia contábil ajustada para o setor. Nossas soluções são apresentadas no formato de “módulos”, cada um com características específicas, que podem ser contratadas considerando as expectativas, o modelo de gestão, o momento e o porte de cada cliente. Além de contabilidade, temos soluções específicas para planejamento e monitoramento de perfomance de redes de negócios.");
+    $("#servImg").removeClass();
+    $("#servImg").addClass("atacImg");
+});
+
+$("#varejistaLink").click(function(e){
+    $("#supermLink, #combustLink, #atacadistaLink, #varejistaLink, #condoLink, #civilLink, #educaLink, #hoteisLink, #industriasLink, #shoppingLink, #prestLink, #agroLink, #saudeLink, #liberalLink").parent().removeClass('active');
+    $("#varejistaLink").parent().addClass('active');
+    $("#tituloServ").text("Comércio Varejista");
+    $("#descServ").text("A importância do comércio varejista no cenário econômico brasileiro é cada vez mais reconhecida. Este é o setor que gera o maior número de empregos formais do país, além de ser também o que recebe o maior número de novos empreendedores. Por outro lado, este é também o segmento concentra o maior número de Micro e Pequenas empresas com mortalidade precoce no país. Destas, apenas um percentual em torno de 20% tem como causa fatores econômicos. Pensando nisto, e nas novas práticas como as normas de contabilidade internacional (IFRS), SPED e e-Social, a NTW Contabilidade e Gestão Empresarial a desenvolveu um série de soluções para apoiar o empreendedor do segmento de varejo. São serviços que vão muito além da contabilidade. Nossas soluções são apresentadas no formato de “módulos”, cada um com características específicas, que podem ser contratadas considerando as expectativas, o modelo de gestão, o momento e o porte de cada cliente. Além de contabilidade, temos soluções específicas para planejamento e monitoramento de perfomance de redes de negócios.");
+    $("#servImg").removeClass();
+    $("#servImg").addClass("varejImg");
+});
+
+$("#condoLink").click(function(e){
+    $("#supermLink, #combustLink, #atacadistaLink, #varejistaLink, #condoLink, #civilLink, #educaLink, #hoteisLink, #industriasLink, #shoppingLink, #prestLink, #agroLink, #saudeLink, #liberalLink").parent().removeClass('active');
+    $("#condoLink").parent().addClass('active');
+    $("#tituloServ").text("Condomínios e Clubes");
+    $("#descServ").text("Nossa solução de contabilidade de Condonínios e Clubes, além de assessoria ao síndico em assuntos financeiros e contábeis, da operação e controle do contas a pagar e receber, garantia do princípio da transparências através da prestação de contas sintética e analítica, obedece os princípios contábeis aplicáveis ao segmento, em especial os previstos na ITG 2002 – Entidade Sem Finalidade de Lucros, aprovada pela Resolução CFC nº 1.409/12. A implantação do e-Social amplia o leque de obrigações e a necessidade de maior controle sobre as relações de trabalho. E para atender todas estas demandas, nossa solução contábil é apresentada no formato de “módulos”, cada um com características bem definidas, que serão avaliadas junto aos nossos clientes considerando o seu modelo de gestão, momento e porte.");
+    $("#servImg").removeClass();
+    $("#servImg").addClass("condoImg");
+});
+
+$("#civilLink").click(function(e){
+    $("#supermLink, #combustLink, #atacadistaLink, #varejistaLink, #condoLink, #civilLink, #educaLink, #hoteisLink, #industriasLink, #shoppingLink, #prestLink, #agroLink, #saudeLink, #liberalLink").parent().removeClass('active');
+    $("#civilLink").parent().addClass('active');
+    $("#tituloServ").text("Construção Civíl");
+    $("#descServ").text("Para a contabilidade de empresas ligadas a Construção Civil, considerando toda a regulação normativa específica para o segmento em função de seu ciclo de produção, financeiro e das modalidades contratuais adotadas no processo de vendas; além do advento de novas práticas como as normas de contabilidade internacional (IFRS), SPED’s e e-Social, a NTW Contabilidade e Gestão Empresarial desenvolveu uma metodologia contábil customizada para o setor. Nossas soluções são apresentadas no formato de “módulos”, cada um com características específicas, que serão definidas considerando as expectativas, o modelo de gestão, o momento e o porte de cada cliente. Expertise no atendimento de Construtoras, Empreiteiras, Incorporadoras, Sistemas de construção em condomínio fechado, empresas de Engenharia e Arquitetura.");
+    $("#servImg").removeClass();
+    $("#servImg").addClass("civilImg");
+});
+
 // Closes the sidebar menu
 $("#menu-close").click(function(e) {
     //e.preventDefault();
@@ -46,13 +101,6 @@ setInterval(function(){
     else if ($header.hasClass("bg5") == true)
     {
         $header.removeClass("bg5");
-        $header.fadeOut("fast");
-        $header.fadeIn("fast");
-        $header.addClass("bg6");
-    }
-    else if ($header.hasClass("bg6") == true)
-    {
-        $header.removeClass("bg6");
         $header.fadeOut("fast");
         $header.fadeIn("fast");
         $header.addClass("bg4");
